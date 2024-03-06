dde7d52be8100e0092e0598c61773787673dca3a
ludovic <ludovic@lemarinel.net>
2024-03-06 18:17:51 +0100

    Created Custom Logger

--- src/Logger.php
+++ src/Logger.php
@@ -3,13 +3,13 @@
 declare(strict_types=1);
 
 namespace Larawatch\Logger;
-use Monolog\Logger;
+use Monolog\Logger as MonologLogger;
 
 class Logger
 {
     public function __invoke(array $config)
     {
-        $logger = new Logger('larawatch');
+        $logger = new MonologLogger('larawatch');
         $logger->pushHandler(new LarawatchHandler());
         $logger->pushProcessor(new RequestProcessor());
 
